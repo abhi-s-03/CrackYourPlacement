cb97c45432cabf395e0bb54121b9af7322c5742e
abhi-s-03 <abhisheks3002@gmail.com>
2024-07-17 18:32:25 +0530

    PrintAnagramsTogether

---
+++ PrintAnagramsTogether.cpp
@@ -0,0 +1,16 @@
+class Solution{
+  public:
+    vector<vector<string> > Anagrams(vector<string>& strs) {
+        //code here
+        unordered_map<string,vector<string>>m;
+        vector<vector<string>> ans;
+        string s;
+        for(int i=0;i<strs.size();i++){
+            s=strs[i];
+            sort(s.begin(),s.end());
+            m[s].push_back(strs[i]);
+        }
+        for(auto x:m) ans.push_back(x.second);
+        return ans;
+    }
+};
