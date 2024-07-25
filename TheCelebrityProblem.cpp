a9a7a7903bd99681f24d492ee10c906f4284bbcc
abhi-s-03 <abhisheks3002@gmail.com>
2024-07-25 07:42:56 +0530

    TheCelebrityProblem

---
+++ TheCelebrityProblem.cpp
@@ -0,0 +1,23 @@
+class Solution 
+{
+    public:
+    //Function to find if there is a celebrity in the party or not.
+    int celebrity(vector<vector<int> >& M, int n) 
+    {
+        int top=0,down=n-1;
+        while(top<down){
+            if(M[top][down]) top++;
+            else if(M[down][top]) down--;
+            else{
+                top++;
+                down--;
+            }
+        }
+        
+        if(top>down) return -1;
+        for(int i=0;i<n;i++){
+            if(i!=top && (M[top][i] || !M[i][top])) return -1;
+        }
+        return top;
+    }
+};
