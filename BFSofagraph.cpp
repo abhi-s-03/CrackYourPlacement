d2a846fdb1118e696bff566836b23e188343a189
abhi-s-03 <abhisheks3002@gmail.com>
2024-08-05 18:59:15 +0530

    BFSofagraph

---
+++ BFSofagraph.cpp
@@ -0,0 +1,24 @@
+class Solution {
+  public:
+    // Function to return Breadth First Traversal of given graph.
+    vector<int> bfsOfGraph(int V, vector<int> adj[]) {
+        // Code here
+        int vis[V]={0};
+        vis[0]=1;
+        queue<int>q;
+        q.push(0);
+        vector<int>ans;
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            ans.push_back(node);
+            for(auto x:adj[node]){
+                if(!vis[x]){
+                    vis[x]=1;
+                    q.push(x);
+                }
+            }
+        }
+        return ans;
+    }
+};
